e7f6ffc83fb9d69670efd9a67baea950756e07fd
Pranav <pranav@geometrylabs.io>
2021-08-06 16:48:41 -0700

    add integration tests

---
+++ tests/transactions_test.go
@@ -0,0 +1,38 @@
+package tests
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestTransactionsEndpoint(t *testing.T) {
+	assert := assert.New(t)
+
+	transactionsServiceURL := os.Getenv("TRANSACTIONS_SERVICE_URL")
+	if transactionsServiceURL == "" {
+		transactionsServiceURL = "http://localhost:8000"
+	}
+	transactionsServiceRestPrefx := os.Getenv("TRANSACTIONS_SERVICE_REST_PREFIX")
+	if transactionsServiceRestPrefx == "" {
+		transactionsServiceRestPrefx = "/api/v1"
+	}
+
+	resp, err := http.Get(transactionsServiceURL + transactionsServiceRestPrefx + "/transactions")
+	assert.Equal(nil, err)
+	assert.Equal(200, resp.StatusCode)
+
+	defer resp.Body.Close()
+
+	bytes, err := ioutil.ReadAll(resp.Body)
+	assert.Equal(nil, err)
+
+	bodyMap := make([]interface{}, 0)
+	err = json.Unmarshal(bytes, &bodyMap)
+	assert.Equal(nil, err)
+	assert.NotEqual(0, len(bodyMap))
+}
