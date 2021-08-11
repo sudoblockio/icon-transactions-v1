233f00610a455ff46e1223dc927c60d33b44faae
Pranav <pranav@geometrylabs.io>
2021-08-10 18:06:38 -0700

    fix x-total-count

--- tests/transactions_test.go
+++ tests/transactions_test.go
@@ -29,7 +29,7 @@ func TestTransactionsEndpoint(t *testing.T) {
 	defer resp.Body.Close()
 
 	// Test headers
-	assert.Equal("0", resp.Header.Get("X-TOTAL-COUNT"))
+	assert.NotEqual("0", resp.Header.Get("X-TOTAL-COUNT"))
 
 	bytes, err := ioutil.ReadAll(resp.Body)
 	assert.Equal(nil, err)
