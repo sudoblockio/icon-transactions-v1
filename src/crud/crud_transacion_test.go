8d0435ab7244f7f8b597d9076efd2fb6b7359ad0
Pranav <pranav@geometrylabs.io>
2021-08-11 13:06:49 -0700

    add crud count test

--- src/crud/crud_transacion_test.go
+++ src/crud/crud_transacion_test.go
@@ -61,32 +61,52 @@ func TestTransactionModelSelect(t *testing.T) {
 	// Select all transactions
 	transactions, err := transactionModel.Select(len(transactionFixtures), 0, "", "", "")
 	assert.Equal(len(transactionFixtures), len(transactions))
-  assert.Equal(nil, err)
+	assert.Equal(nil, err)
 
 	// Test limit
 	transactions, err = transactionModel.Select(1, 0, "", "", "")
 	assert.Equal(1, len(transactions))
-  assert.Equal(nil, err)
+	assert.Equal(nil, err)
 
 	// Test skip
 	transactions, err = transactionModel.Select(1, 1, "", "", "")
 	assert.Equal(1, len(transactions))
-  assert.Equal(nil, err)
+	assert.Equal(nil, err)
 
 	// Test hash
 	transactions, err = transactionModel.Select(1, 0, "0x18094ca8e7f5cc52511c36a8c85f56c8788b8802025a8cbcd84fb0f5b5ea7d82", "", "")
 	assert.Equal(1, len(transactions))
-  assert.Equal(nil, err)
+	assert.Equal(nil, err)
 
 	// Test from
-  transactions, err = transactionModel.Select(1, 0, "", "hx02e6bf5860b7d7744ec5050545d10d37c72ac2ef", "")
+	transactions, err = transactionModel.Select(1, 0, "", "hx02e6bf5860b7d7744ec5050545d10d37c72ac2ef", "")
 	assert.Equal(1, len(transactions))
-  assert.Equal(nil, err)
+	assert.Equal(nil, err)
 
 	// Test to
 	transactions, err = transactionModel.Select(1, 0, "", "", "cx38fd2687b202caf4bd1bda55223578f39dbb6561")
 	assert.Equal(1, len(transactions))
-  assert.Equal(nil, err)
+	assert.Equal(nil, err)
+}
+
+func TestTransactionModelCountAll(t *testing.T) {
+	assert := assert.New(t)
+
+	transactionModel := GetTransactionModel()
+	assert.NotEqual(nil, transactionModel)
+
+	migrateErr := transactionModel.Migrate()
+	assert.Equal(nil, migrateErr)
+
+	// Load fixtures
+	transactionFixtures := fixtures.LoadTransactionFixtures()
+	for _, transaction := range transactionFixtures {
+		insertErr := transactionModel.Insert(transaction)
+		assert.Equal(nil, insertErr)
+	}
+
+	count := transactionModel.CountAll()
+	assert.NotEqual(0, count)
 }
 
 func TestTransactionModelLoader(t *testing.T) {
@@ -114,5 +134,5 @@ func TestTransactionModelLoader(t *testing.T) {
 	// Select all transactions
 	transactions, err := transactionModel.Select(len(transactionFixtures), 0, "", "", "")
 	assert.Equal(len(transactionFixtures), len(transactions))
-  assert.Equal(nil, err)
+	assert.Equal(nil, err)
 }
