e95e0e529e440f3a8476d57dec41037d07713c0c
Pranav <pranav@geometrylabs.io>
2021-09-28 20:41:25 -0700

    add redis retry logic

--- src/redis/redis.go
+++ src/redis/redis.go
@@ -2,9 +2,11 @@ package redis
 
 import (
 	"context"
+	"errors"
 	"sync"
 	"time"
 
+	"github.com/cenkalti/backoff"
 	"github.com/go-redis/redis/v8"
 	"go.uber.org/zap"
 
@@ -23,44 +25,56 @@ func GetRedisClient() *Client {
 	redisClientOnce.Do(func() {
 		addr := config.Config.RedisHost + ":" + config.Config.RedisPort
 
-		redisClient = new(Client)
+		retryOperation := func() error {
+			redisClient = new(Client)
 
-		// Init connection
-		if config.Config.RedisSentinelClientMode == false {
-			// Use default client
-			redisClient.client = redis.NewClient(&redis.Options{
-				Addr:     addr,
-				Password: config.Config.RedisPassword,
-				DB:       0,
-			})
-		} else {
-			// Use sentinel client
-			redisClient.client = redis.NewFailoverClient(&redis.FailoverOptions{
-				MasterName:    config.Config.RedisSentinelClientMasterName,
-				SentinelAddrs: []string{addr},
-			})
+			// Init connection
+			if config.Config.RedisSentinelClientMode == false {
+				// Use default client
+				redisClient.client = redis.NewClient(&redis.Options{
+					Addr:     addr,
+					Password: config.Config.RedisPassword,
+					DB:       0,
+				})
+			} else {
+				// Use sentinel client
+				redisClient.client = redis.NewFailoverClient(&redis.FailoverOptions{
+					MasterName:    config.Config.RedisSentinelClientMasterName,
+					SentinelAddrs: []string{addr},
+				})
+			}
+
+			if redisClient.client == nil {
+				zap.S().Warn("RedisClient: Unable to create to redis client")
+				return errors.New("RedisClient: Unable to create to redis client")
+			}
+
+			ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+			defer cancel()
+
+			// Test connection
+			_, err := redisClient.client.Ping(ctx).Result()
+			if err != nil {
+				zap.S().Warn("RedisClient: Unable to connect to redis", err.Error())
+				return err
+			}
+
+			// Init pubsub
+			redisClient.pubsub = redisClient.client.Subscribe(ctx, config.Config.RedisChannel)
+
+			// Test pubsub
+			_, err = redisClient.pubsub.Receive(ctx)
+			if err != nil {
+				zap.S().Warn("RedisClient: Unable to create pubsub channel")
+				return err
+			}
+
+			return nil
 		}
 
-		if redisClient.client == nil {
-			zap.S().Fatal("RedisClient: Unable to create to redis client")
-		}
-
-		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-		defer cancel()
-
-		// Test connection
-		_, err := redisClient.client.Ping(ctx).Result()
+		err := backoff.Retry(retryOperation, backoff.NewConstantBackOff(time.Second*3))
 		if err != nil {
-			zap.S().Fatal("RedisClient: Unable to connect to redis", err.Error())
-		}
-
-		// Init pubsub
-		redisClient.pubsub = redisClient.client.Subscribe(ctx, config.Config.RedisChannel)
-
-		// Test pubsub
-		_, err = redisClient.pubsub.Receive(ctx)
-		if err != nil {
-			zap.S().Fatal("RedisClient: Unable to create pubsub channel")
+			zap.S().Fatal("RedisClient: Could not connect to redis service ERROR=", err.Error())
 		}
 	})
 
