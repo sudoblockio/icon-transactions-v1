ada77f72bc833ae029d7a78125c161ccb7dfb127
Pranav <pranav@geometrylabs.io>
2021-10-13 14:24:32 -0700

    add max idle and max open gorm connections to env vars

--- src/config/config.go
+++ src/config/config.go
@@ -51,14 +51,16 @@ type configType struct {
 	SchemaFolderPath             string            `envconfig:"SCHEMA_FOLDER_PATH" required:"false" default:"schemas/"`
 
 	// DB
-	DbDriver   string `envconfig:"DB_DRIVER" required:"false" default:"postgres"`
-	DbHost     string `envconfig:"DB_HOST" required:"false" default:"localhost"`
-	DbPort     string `envconfig:"DB_PORT" required:"false" default:"5432"`
-	DbUser     string `envconfig:"DB_USER" required:"false" default:"postgres"`
-	DbPassword string `envconfig:"DB_PASSWORD" required:"false" default:"changeme"`
-	DbName     string `envconfig:"DB_DBNAME" required:"false" default:"postgres"`
-	DbSslmode  string `envconfig:"DB_SSL_MODE" required:"false" default:"disable"`
-	DbTimezone string `envconfig:"DB_TIMEZONE" required:"false" default:"UTC"`
+	DbDriver             string `envconfig:"DB_DRIVER" required:"false" default:"postgres"`
+	DbHost               string `envconfig:"DB_HOST" required:"false" default:"localhost"`
+	DbPort               string `envconfig:"DB_PORT" required:"false" default:"5432"`
+	DbUser               string `envconfig:"DB_USER" required:"false" default:"postgres"`
+	DbPassword           string `envconfig:"DB_PASSWORD" required:"false" default:"changeme"`
+	DbName               string `envconfig:"DB_DBNAME" required:"false" default:"postgres"`
+	DbSslmode            string `envconfig:"DB_SSL_MODE" required:"false" default:"disable"`
+	DbTimezone           string `envconfig:"DB_TIMEZONE" required:"false" default:"UTC"`
+	DbMaxIdleConnections int    `envconfig:"DB_MAX_IDLE_CONNECTIONS" required:"false" default:"2"`
+	DbMaxOpenConnections int    `envconfig:"DB_MAX_OPEN_CONNECTIONS" required:"false" default:"10"`
 
 	// Redis
 	RedisHost                     string `envconfig:"REDIS_HOST" required:"false" default:"redis"`
