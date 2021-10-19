c52f8214f1ec568aa681e010c6e125d44312c029
Pranav <pranav@geometrylabs.io>
2021-10-19 14:46:33 -0700

    change tail to read kafka job ids from postgress

--- src/config/config.go
+++ src/config/config.go
@@ -45,8 +45,9 @@ type configType struct {
 	KafkaGroupID      string `envconfig:"KAFKA_GROUP_ID" required:"false" default:"transactions-service"`
 
 	// Topics
-	ConsumerGroupHead            string            `envconfig:"CONSUMER_GROUP_HEAD" required:"false" default:"transactions-consumer-group-head"`
-	ConsumerGroupTail            string            `envconfig:"CONSUMER_GROUP_TAIL" required:"false" default:"transactions-consumer-group-tail"`
+	ConsumerGroup                string            `envconfig:"CONSUMER_GROUP" required:"false" default:"transactions-consumer-group"`
+	ConsumerIsTail               bool              `envconfig:"CONSUMER_IS_TAIL" required:"false" default:"false"`
+	ConsumerJobID                string            `envconfig:"CONSUMER_JOB_ID" required:"false" default:""`
 	ConsumerGroupBalanceStrategy string            `envconfig:"CONSUMER_GROUP_BALANCE_STRATEGY" required:"false" default:"BalanceStrategySticky"`
 	ConsumerTopicBlocks          string            `envconfig:"CONSUMER_TOPIC_BLOCKS" required:"false" default:"blocks"`
 	ConsumerTopicTransactions    string            `envconfig:"CONSUMER_TOPIC_TRANSACTIONS" required:"false" default:"transactions"`
