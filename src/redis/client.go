f507c467a03ece270e102ad29a54b22d3a0cafa2
Pranav <pranav@geometrylabs.io>
2021-10-20 15:25:45 -0700

    add non-blocking loop to redis broadcaster

--- src/redis/client.go
+++ src/redis/client.go
@@ -80,37 +80,3 @@ func GetRedisClient() *Client {
 
 	return redisClient
 }
-
-func (c *Client) Publish(data []byte) {
-	for {
-		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-		defer cancel()
-
-		// Publish
-		err := c.client.Publish(ctx, config.Config.RedisChannel, string(data)).Err()
-		if err != nil {
-			// Failure
-			zap.S().Warn("Redis Publish: Cannot publish message...retrying in 3 second")
-			time.Sleep(3 * time.Second)
-
-			continue
-		}
-
-		// Success
-		break
-	}
-}
-
-func (c *Client) StartSubscriber() {
-
-	go func() {
-		subscriberChannel := c.pubsub.Channel()
-		inputChannel := GetBroadcaster().InputChannel
-
-		for {
-			redisMsg := <-subscriberChannel
-
-			inputChannel <- []byte(redisMsg.Payload)
-		}
-	}()
-}
