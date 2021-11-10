9333156e89f9f779478aa16a0407800b9591568f
Pranav <pranav@geometrylabs.io>
2021-11-09 20:12:35 -0800

    add transaction token transferby address integration tests

---
+++ tests/transactions_token_transfer_address_test.go
@@ -0,0 +1,61 @@
+package tests
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// Address test
+func TestTransactionsTokenTransferAddressEndpoint(t *testing.T) {
+	assert := assert.New(t)
+
+	transactionsServiceURL := os.Getenv("TRANSACTIONS_SERVICE_URL")
+	if transactionsServiceURL == "" {
+		transactionsServiceURL = "http://localhost:8000"
+	}
+	transactionsServiceRestPrefx := os.Getenv("TRANSACTIONS_SERVICE_REST_PREFIX")
+	if transactionsServiceRestPrefx == "" {
+		transactionsServiceRestPrefx = "/api/v1"
+	}
+
+	// Get latest transaction
+	resp, err := http.Get(transactionsServiceURL + transactionsServiceRestPrefx + "/transactions/token-transfers?limit=1")
+	assert.Equal(nil, err)
+	assert.Equal(200, resp.StatusCode)
+
+	defer resp.Body.Close()
+
+	// Test headers
+	assert.NotEqual("0", resp.Header.Get("X-TOTAL-COUNT"))
+
+	bytes, err := ioutil.ReadAll(resp.Body)
+	assert.Equal(nil, err)
+
+	bodyMap := make([]interface{}, 0)
+	err = json.Unmarshal(bytes, &bodyMap)
+	assert.Equal(nil, err)
+	assert.NotEqual(0, len(bodyMap))
+
+	// Get testable address
+	transactionFromAddress := bodyMap[0].(map[string]interface{})["from_address"].(string)
+
+	// Test number
+	resp, err = http.Get(transactionsServiceURL + transactionsServiceRestPrefx + "/transactions/token-transfers/address/" + transactionFromAddress)
+	assert.Equal(nil, err)
+	assert.Equal(200, resp.StatusCode)
+
+	defer resp.Body.Close()
+
+	bytes, err = ioutil.ReadAll(resp.Body)
+	assert.Equal(nil, err)
+
+	bodyMap = make([]interface{}, 0)
+	err = json.Unmarshal(bytes, &bodyMap)
+	assert.Equal(nil, err)
+	assert.NotEqual(0, len(bodyMap))
+}
